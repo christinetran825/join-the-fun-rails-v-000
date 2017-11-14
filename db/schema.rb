82f15147916059d4dcb7ab5c63829c86056622b8
Christine Tran <christinetran825@gmail.com>
2017-11-14 06:04:37 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20171114060340) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "passengers_taxis", id: false, force: :cascade do |t|
+    t.integer "taxi_id",      null: false
+    t.integer "passenger_id", null: false
+  end
+
   create_table "rides", force: :cascade do |t|
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
